58ba9b9879c1a95d28615b47d767484572ca19b5
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-19 16:05:42 +0000

    can add book details using swiftdata

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,71 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Yashavika Singh on 19.06.24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    @State private var rating = 3
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    var body: some View {
+        NavigationStack{
+            Form{
+               
+                Section{
+                    TextField("Author", text: $author)
+                    TextField("Title", text: $title)
+
+                    Picker("genre", selection: $genre)
+                    {
+                        ForEach(genres, id: \.self){
+                            genre in Text(genre)
+                        }
+                    }
+                }
+                    
+                
+                
+                Section("Write a review"){
+                    TextField("Review", text: $review)
+                    
+                    Picker("Rating", selection: $rating)
+                    {
+                        ForEach(0..<6){
+                            Text(String($0))
+                        }
+                    }
+                    
+                }
+                
+                Section{
+                    Button("Save"){
+                        let newBook =  Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                        dismiss()
+                    }
+                }
+          
+                     
+                
+               
+            }.navigationTitle("Add book")
+        }
+    
+       
+    }
+}
+
+//#Preview {
+//    AddBookView()
+//}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -4,18 +4,32 @@
 //
 //  Created by Yashavika Singh on 18.06.24.
 //
-
+import SwiftData
 import SwiftUI
 
+
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @State private var showingAddScreen  = false
+    @Query var books: [Book]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Text("Count: \(books.count)").navigationTitle("BookWorm")
+                .toolbar(){
+                    ToolbarItem(placement: .topBarTrailing)
+                    {
+                        Button("Add Book", systemImage: "plus"){
+                            showingAddScreen.toggle()
+                        }
+                    }
+                   
+                }
+            
+                .sheet(isPresented: $showingAddScreen, content: {
+                   AddBookView()
+                })
         }
-        .padding()
+  
     }
 }
 
